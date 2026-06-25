c3d2cf3cb093a2f91a6f8b8933a3afa0fe706f1e
addicionalseo <addicionalseo@gmail.com>
2026-06-25 18:20:00 +0200

    Inclou versio ES al build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        es: resolve(__dirname, 'es/index.html')
+      }
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
